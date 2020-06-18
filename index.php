daa8a4c8a35228f38da178204e978d39e4aec4f7
FauziAris <46512994+FauziAris@users.noreply.github.com>
2020-06-18 14:22:05 +0700

    create connect db  and page index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Halaman utama</title>
+</head>
+
+<body>
+  <?php
+  require_once "koneksi.php";
+
+  ?>
+  <h1>Data Pegawai</h1>
+</body>
+
+</html>
