cf1329a0a340e20c4bf3e80b16b50cc4d3994ed6
FauziAris <46512994+FauziAris@users.noreply.github.com>
2020-06-18 14:24:57 +0700

    add code: show data form table on index page

--- index.php
+++ index.php
@@ -11,8 +11,36 @@
   <?php
   require_once "koneksi.php";
 
+  $SQL = "SELECT * FROM tb_pegawai";
+  $result = $conn->query($SQL);
   ?>
   <h1>Data Pegawai</h1>
+  <hr>
+  <table border="1">
+    <tr>
+      <th>KODE PEGAWAI</th>
+      <th>NAMA</th>
+      <th>JABATAN</th>
+      <th>GAJI POKOK</th>
+      <th>TRANPORTASI</th>
+      <th>GAJI BERSIH</th>
+      <th>ACTION</th>
+    </tr>
+    <?php while ($d = $result->fetch_assoc()) : ?>
+      <tr>
+        <td><?= $d['kode_pegawai'] ?></td>
+        <td><?= $d['nama'] ?></td>
+        <td><?= $d['jabatan'] ?></td>
+        <td>Rp.<?= number_format($d['gaji_pokok'], 0, ',', '.') ?></td>
+        <td>Rp.<?= number_format($d['transportasi'], 0, ',', '.') ?></td>
+        <td>Rp.<?= number_format($d['gaji_bersih'], 0, ',', '.') ?></td>
+        <td>
+          <a href=""><button>Update</button></a>
+          <a href=""><button>Delete</button></a>
+        </td>
+      </tr>
+    <?php endwhile; ?>
+  </table>
 </body>
 
 </html>
